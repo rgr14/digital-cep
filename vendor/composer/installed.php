1f8268047af4ceb8e15c3c79ac4b60705c29e50f
Roger Augusto <56980703+rgr14@users.noreply.github.com>
2022-12-29 12:49:47 -0300

    Busca Inicial no Via Cep

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bdd27924fd5c25ab1069efd4de9dec1e612214c1',
+        'name' => 'roger/digital-cep',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'roger/digital-cep' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bdd27924fd5c25ab1069efd4de9dec1e612214c1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
